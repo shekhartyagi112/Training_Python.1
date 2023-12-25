9c4794135d7ef9d0955a84c2f8674d27989f2207
Shekhartyagi112 <shekhartyagi53@gmail.com>
2023-12-25 17:56:57 +0530

    Advance python

--- Amazone.py
+++ Amazone.py
@@ -2,10 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import time
-options= Options
-options.add_experimental_option("detach", True )
-driver= webdriver.Chrome(options=options)
+options= Options ()
+options.add_experimental_option("detach",True)
+driver= webdriver.Chrome()
 driver.maximize_window()
 driver.implicitly_wait(5)
 driver.get("https://www.amazon.in/")
-search_box= driver.find_element(By.)
+#search_box= driver.find_element(By.)

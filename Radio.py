0d4903ddb95ce1b26d8b78db84876139ff7a92b3
Shekhartyagi112 <shekhartyagi53@gmail.com>
2024-01-30 12:13:02 +0530

    Selenium Code

--- Radio.py
+++ Radio.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.select import Select
+
 #Opetion is use for stop the chrome page after complete script, it will not close automatically.
 
 options = Options()
